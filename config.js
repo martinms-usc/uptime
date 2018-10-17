6111d9be5eccd3706cb9b375564b5e7fd52b0f0b
Michael Martin <martinms.usc@gmail.com>
2018-10-17 16:03:41 -0700

    add https support

---
+++ config.js
@@ -0,0 +1,19 @@
+const environments = {};
+
+// 80, 443
+environments.staging = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'staging'
+};
+
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production'
+};
+
+const currentEnv = typeof process.env.NODE_ENV == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+const env = typeof environments[currentEnv] == 'object' ? environments[currentEnv] : environments.staging;
+
+module.exports = env;
--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const http = require("http");
+const https = require("https");
 const url = require("url");
+const fs = require("fs");
 const StringDecoder = require("string_decoder").StringDecoder;
+const config = require("./config");
+const PORT = config.httpPort;
+const HTTPS_PORT = config.httpsPort;
 
-const PORT = 3001;
+// instantiate http server
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res);
+});
 
-const server = http.createServer((req, res) => {
+// start server
+httpServer.listen(PORT, () => {
+  console.log(`[${config.envName.toUpperCase()}] server is listening on port: ${PORT}`);
+});
+
+
+// instantiate https server
+const httpsServerOpts = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/cert.pem')
+};
+
+const httpsServer = https.createServer(httpsServerOpts, (req, res) => {
+  unifiedServer(req, res);
+});
+
+// start https server
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`[${config.envName.toUpperCase()}] server is listening on port: ${HTTPS_PORT}`);
+});
+
+
+const unifiedServer = function (req, res) {
   let parsedUrl = url.parse(req.url, true);
   let path = parsedUrl.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g, '');
@@ -35,20 +65,14 @@ const server = http.createServer((req, res) => {
     // route request
     handler(data, (statusCode = 200, payload = {}) => {
       let payloadString = JSON.stringify(payload);
-      // send the response
+      res.setHeader('Content-Type', 'application/json' );
       res.writeHead(statusCode);
       res.end(payloadString);
     
-      // log request
       console.log('request returned with ', statusCode, payloadString);
     });
   });
-});
-
-
-server.listen(PORT, () => {
-  console.log(`the server is listening on port ${PORT}`);
-});
+};
 
 
 // handlers
@@ -59,7 +83,7 @@ handlers.sample = (data, callback) => {
 };
 
 handlers.notFound = (data, callback) => {
-  callback(404);
+  callback(404, { error: 'route' });
 };
 
 // router
